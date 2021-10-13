3911a09ad7bd04a6eadfa8b72f10abf590d77f8b
Priya Potti <59410993+Priya378@users.noreply.github.com>
2021-10-13 09:50:10 +0530

    Modify expected answer for quiz 2

--- Source Codes/expected_answer_for_quiz2.rkt
+++ Source Codes/expected_answer_for_quiz2.rkt
@@ -1,12 +1,13 @@
 #lang racket
 ;Define a procedure to get the count of numbers divisible by 3 in a given tree
+
 (define (div n)
-  (= (modulo n 3) 0))
+(= (remainder n 3) 0))
 
 (define (count tr)
-  (cond ((null? tr) 0)
-        ((pair? tr) (+ (count (car tr)) (count (cdr tr))))
-        ((div tr) (+ 1 (count (cdr tr))))
-        (else (count (cdr tr)))))
+(cond ((null? tr) 0)
+((pair? tr)(+ (count (car tr)) (count (cdr tr))))
+((div tr) 1)
+(else 0)))
 
-(count '(1 2 (3 4) 5 11 6 12 (7 (8 9)))) 
+(count '(1 2 (3 4) 5 11 6 12 (7 (8 9))))
